385848f3a48eef5cf2a63094c918502170532e03
Viktoria <doduchenko29@gmail.com>
2021-07-11 21:40:46 +0300

    add

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Page from './Page.jsx';
+
+const rootElemnt = document.querySelector('#root');
+
+ReactDOM.render(<Page />, rootElemnt);
