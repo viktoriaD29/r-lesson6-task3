60327776287b441a52eda5d3924af047677d9f23
Viktoria <doduchenko29@gmail.com>
2021-07-11 21:43:30 +0300

    add

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,36 +1,41 @@
-import React, { Component } from 'react'
-import Message from './Message.jsx'
+import React from 'react';
+import Message from './Message.jsx';
 
 const text1 = 'Hello, world!';
-const text2 = 'Another exciting text';
+const text2 = 'Another exciting text.';
 
-class Page extends Component {
+class Page extends React.Component {
   constructor(props) {
-    super(props) 
+    super(props);
 
     this.state = {
-      text: null
-    }
+      text: null,
+    };
   }
 
   setText = (text) => {
     this.setState({
-      text
-    })
-  }
+      text,
+    });
+  };
 
   render() {
     return (
       <div className="page">
-        <Message text={this.state.text}/>
+        <Message text={this.state.text} />
         <div className="actions">
-          <button className="btn" onClick={() => this.setText(text1)}>Text 1</button>
-          <button className="btn" onClick={() => this.setText(text2)}>Text 2</button>
-          <button className="btn" onClick={() => this.setText('')}>Clear</button>
+          <button className="btn" onClick={() => this.setText(text1)}>
+            Text 1
+          </button>
+          <button className="btn" onClick={() => this.setText(text2)}>
+            Text 2
+          </button>
+          <button className="btn" onClick={() => this.setText('')}>
+            Clear
+          </button>
         </div>
       </div>
     );
   }
 }
-
-export default Page
+export default Page;
